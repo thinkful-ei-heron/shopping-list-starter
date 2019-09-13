3083abd365664e7206cb452a6cc11e0e2636d743
Andrea Cardy Bailey <andrea.c.bailey@gmail.com>
2019-09-13 15:47:28 -0400

    Adding imports for testing to index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import 'normalize.css';
 import './index.css';
 
 import shoppingList from './shopping-list';
+import store from './store.js';
+import item from './item.js';
 
 function main() {
   shoppingList.bindEventListeners();
