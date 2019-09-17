0f463f4bef2d2713c5bcdf824881db743b59ea12
Andrea Cardy Bailey <andrea.c.bailey@gmail.com>
2019-09-16 21:02:27 -0400

    Normalizing formatting

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 import shoppingList from './shopping-list.js';
 
-function main() {
+const main = function () {
   shoppingList.bindEventListeners();
   shoppingList.render();
 }
--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -3,7 +3,7 @@ const store = {
   hideCheckedItems: false
 };
 
-function generateItemElement(item) {
+const generateItemElement = function (item) {
   let itemTitle = `<span class="shopping-item shopping-item__checked">${item.name}</span>`;
   if (!item.checked) {
     itemTitle = `
@@ -25,16 +25,14 @@ function generateItemElement(item) {
         </button>
       </div>
     </li>`;
-}
+};
 
-
-function generateShoppingItemsString(shoppingList) {
+const generateShoppingItemsString = function (shoppingList) {
   const items = shoppingList.map((item) => generateItemElement(item));
   return items.join('');
-}
+};
 
-
-function render() {
+const render = function () {
   // Filter item list if store prop is true by item.checked === false
   let items = [...store.items];
   if (store.hideCheckedItems) {
@@ -46,14 +44,13 @@ function render() {
 
   // insert that HTML into the DOM
   $('.js-shopping-list').html(shoppingListItemsString);
-}
+};
 
-
-function addItemToShoppingList(itemName) {
+const addItemToShoppingList = function (itemName) {
   store.items.push({ id: cuid(), name: itemName, checked: false });
-}
+};
 
-function handleNewItemSubmit() {
+const handleNewItemSubmit = function () {
   $('#js-shopping-list-form').submit(function (event) {
     event.preventDefault();
     const newItemName = $('.js-shopping-list-entry').val();
@@ -61,43 +58,43 @@ function handleNewItemSubmit() {
     addItemToShoppingList(newItemName);
     render();
   });
-}
+};
 
-function toggleCheckedForListItem(id) {
+const toggleCheckedForListItem = function (id) {
   const foundItem = store.items.find(item => item.id === id);
   foundItem.checked = !foundItem.checked;
-}
+};
 
 
-function getItemIdFromElement(item) {
+const getItemIdFromElement = function (item) {
   return $(item)
     .closest('.js-item-element')
     .data('item-id');
-}
+};
 
-function handleItemCheckClicked() {
+const handleItemCheckClicked = function () {
   $('.js-shopping-list').on('click', '.js-item-toggle', event => {
     const id = getItemIdFromElement(event.currentTarget);
     toggleCheckedForListItem(id);
     render();
   });
-}
+};
 
-function deleteListItem(id) {
+const deleteListItem = function (id) {
   const index = store.items.findIndex(item => item.id === id);
   store.items.splice(index, 1);
-}
+};
 
-function editListItemName(id, itemName) {
+const editListItemName = function (id, itemName) {
   const item = store.items.find(item => item.id === id);
   item.name = itemName;
-}
+};
 
-function toggleCheckedItemsFilter() {
+const toggleCheckedItemsFilter = function () {
   store.hideCheckedItems = !store.hideCheckedItems;
-}
+};
 
-function handleDeleteItemClicked() {
+const handleDeleteItemClicked = function () {
   // like in `handleItemCheckClicked`, we use event delegation
   $('.js-shopping-list').on('click', '.js-item-delete', event => {
     // get the index of the item in store.items
@@ -107,9 +104,9 @@ function handleDeleteItemClicked() {
     // render the updated shopping list
     render();
   });
-}
+};
 
-function handleEditShoppingItemSubmit() {
+const handleEditShoppingItemSubmit = function () {
   $('.js-shopping-list').on('submit', '.js-edit-item', event => {
     event.preventDefault();
     const id = getItemIdFromElement(event.currentTarget);
@@ -117,22 +114,22 @@ function handleEditShoppingItemSubmit() {
     editListItemName(id, itemName);
     render();
   });
-}
+};
 
-function handleToggleFilterClick() {
+const handleToggleFilterClick = function () {
   $('.js-filter-checked').click(() => {
     toggleCheckedItemsFilter();
     render();
   });
-}
+};
 
-function bindEventListeners() {
+const bindEventListeners = function () {
   handleNewItemSubmit();
   handleItemCheckClicked();
   handleDeleteItemClicked();
   handleEditShoppingItemSubmit();
   handleToggleFilterClick();
-}
+};
 
 // This object contains the only exposed methods from this module:
 export default {
