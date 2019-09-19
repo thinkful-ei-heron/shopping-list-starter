efac5f3461f53195e8f0b025e3375fe480938956
Andrea Cardy Bailey <andrea.c.bailey@gmail.com>
2019-09-19 13:22:30 -0400

    Updating starter files to match course example. TODO: Update with-store.

---
+++ index.js
@@ -0,0 +1,123 @@
+const store = {
+  items: [
+    { id: cuid(), name: 'apples', checked: false },
+    { id: cuid(), name: 'oranges', checked: false },
+    { id: cuid(), name: 'milk', checked: true },
+    { id: cuid(), name: 'bread', checked: false }
+  ],
+  hideCheckedItems: false
+};
+
+const generateItemElement = function (item) {
+  let itemTitle = `<span class='shopping-item shopping-item__checked'>${item.name}</span>`;
+  if (!item.checked) {
+    itemTitle = `
+      <form class='js-edit-item'>
+        <input class='shopping-item' type='text' value='${item.name}' />
+      </form>
+    `;
+  }
+
+  return `
+    <li class='js-item-element' data-item-id='${item.id}'>
+      ${itemTitle}
+      <div class='shopping-item-controls'>
+        <button class='shopping-item-toggle js-item-toggle'>
+          <span class='button-label'>check</span>
+        </button>
+        <button class='shopping-item-delete js-item-delete'>
+          <span class='button-label'>delete</span>
+        </button>
+      </div>
+    </li>`;
+};
+
+const generateShoppingItemsString = function (shoppingList) {
+  const items = shoppingList.map((item) => generateItemElement(item));
+  return items.join('');
+};
+
+const render = function () {
+  // Filter item list if store prop is true by item.checked === false
+  let items = [...store.items];
+  if (store.hideCheckedItems) {
+    items = items.filter(item => !item.checked);
+  }
+
+  // render the shopping list in the DOM
+  const shoppingListItemsString = generateShoppingItemsString(items);
+
+  // insert that HTML into the DOM
+  $('.js-shopping-list').html(shoppingListItemsString);
+};
+
+const addItemToShoppingList = function (itemName) {
+  store.items.push({ id: cuid(), name: itemName, checked: false });
+};
+
+const handleNewItemSubmit = function () {
+  $('#js-shopping-list-form').submit(function (event) {
+    event.preventDefault();
+    const newItemName = $('.js-shopping-list-entry').val();
+    $('.js-shopping-list-entry').val('');
+    addItemToShoppingList(newItemName);
+    render();
+  });
+};
+
+const toggleCheckedForListItem = function (id) {
+  const foundItem = store.items.find(item => item.id === id);
+  foundItem.checked = !foundItem.checked;
+};
+
+const getItemIdFromElement = function (item) {
+  return $(item)
+    .closest('.js-item-element')
+    .data('item-id');
+};
+
+const handleItemCheckClicked = function () {
+  $('.js-shopping-list').on('click', '.js-item-toggle', event => {
+    const id = getItemIdFromElement(event.currentTarget);
+    toggleCheckedForListItem(id);
+    render();
+  });
+};
+
+const toggleCheckedItemsFilter = function () {
+  store.hideCheckedItems = !store.hideCheckedItems;
+};
+
+const deleteListItem = function (id) {
+  const index = store.items.findIndex(item => item.id === id);
+  store.items.splice(index, 1);
+};
+
+const handleDeleteItemClicked = function () {
+  // like in `handleItemCheckClicked`, we use event delegation
+  $('.js-shopping-list').on('click', '.js-item-delete', event => {
+    // get the index of the item in store.items
+    const id = getItemIdFromElement(event.currentTarget);
+    // delete the item
+    deleteListItem(id);
+    // render the updated shopping list
+    render();
+  });
+};
+
+const handleToggleFilterClick = function () {
+  $('.js-filter-checked').click(() => {
+    toggleCheckedItemsFilter();
+    render();
+  });
+};
+
+const handleShoppingList = function () {
+  render();
+  handleNewItemSubmit();
+  handleItemCheckClicked();
+  handleDeleteItemClicked();
+  handleToggleFilterClick();
+};
+
+$(handleShoppingList);
