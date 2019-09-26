52e3f2450d4d23c719dc646a7209dc5ecfdc568a
Capi Etheriel <barraponto@gmail.com>
2019-09-26 16:24:31 -0300

    Make every variable  a property of store object

--- scripts/store.js
+++ scripts/store.js
@@ -1,51 +1,55 @@
 import item from './item.js';
 
-const items = [];
-let hideCheckeditems = false;
+const store = {
+  items: [],
+  hideCheckeditems: false,
 
-const findById = function (id) {
-  return this.items.find(currentItem => currentItem.id === id);
-};
-
-const addItem = function (name) {
-  try {
-    item.validateName(name);
-    this.items.push(item.create(name));
-  } catch (e) {
-    console.log(e.message);
-  }
-};
-
-const findAndToggleChecked = function (id) {
-  const currentItem = this.findById(id);
-  currentItem.checked = !currentItem.checked;
-};
-
-const findAndUpdateName = function (id, name) {
-  try {
-    item.validateName(name);
+  findById: function (id) {
+    return this.items.find(currentItem => currentItem.id === id);
+  },
+  
+  
+  addItem: function (name) {
+    try {
+      item.validateName(name);
+      this.items.push(item.create(name));
+    } catch (e) {
+      console.log(e.message);
+    }
+  },
+  
+  findAndToggleChecked: function (id) {
     const currentItem = this.findById(id);
-    currentItem.name = name;
-  } catch (e) {
-    console.log('Cannot update name: ' + e.message);
-  }
+    currentItem.checked = !currentItem.checked;
+  },
+  
+  findAndUpdateName: function (id, name) {
+    try {
+      item.validateName(name);
+      const currentItem = this.findById(id);
+      currentItem.name = name;
+    } catch (e) {
+      console.log('Cannot update name: ' + e.message);
+    }
+  },
+  
+  findAndUpdateName: function (id, name) {
+    try {
+      item.validateName(name);
+      const currentItem = this.findById(id);
+      currentItem.name = name;
+    } catch (e) {
+      console.log('Cannot update name: ' + e.message);
+    }
+  },
+  
+  findAndDelete: function (id) {
+    this.items = this.items.filter(currentItem => currentItem.id !== id);
+  },
+  
+  toggleCheckedFilter: function () {
+    this.hideCheckedItems = !this.hideCheckedItems;
+  }  
 };
 
-const findAndDelete = function (id) {
-  this.items = this.items.filter(currentItem => currentItem.id !== id);
-};
-
-const toggleCheckedFilter = function () {
-  this.hideCheckedItems = !this.hideCheckedItems;
-};
-
-export default {
-  items,
-  hideCheckeditems,
-  findById,
-  addItem,
-  findAndToggleChecked,
-  findAndUpdateName,
-  findAndDelete,
-  toggleCheckedFilter
-};
+export default store;
