fb4b24c72c64cbeb76a9a5b0204aa7991d541f39
Andrea Cardy Bailey <andrea.c.bailey@gmail.com>
2019-09-26 13:17:33 -0400

    Updating naming conflicts form 'item' in store.js

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ const items = [];
 let hideCheckeditems = false;
 
 const findById = function (id) {
-  return this.items.find(item => item.id === id);
+  return this.items.find(currentItem => currentItem.id === id);
 };
 
 const addItem = function (name) {
@@ -17,22 +17,22 @@ const addItem = function (name) {
 };
 
 const findAndToggleChecked = function (id) {
-  const item = this.findById(id);
-  item.checked = !item.checked;
+  const currentItem = this.findById(id);
+  currentItem.checked = !currentItem.checked;
 };
 
 const findAndUpdateName = function (id, name) {
   try {
     item.validateName(name);
-    const item = this.findById(id);
-    item.name = name;
+    const currentItem = this.findById(id);
+    currentItem.name = name;
   } catch (e) {
     console.log('Cannot update name: ' + e.message);
   }
 };
 
 const findAndDelete = function (id) {
-  this.items = this.items.filter(item => item.id !== id);
+  this.items = this.items.filter(currentItem => currentItem.id !== id);
 };
 
 const toggleCheckedFilter = function () {
