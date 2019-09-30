7ac08d2b3dc89aa1ea08c7d42d5d5dd0bf02b79e
Andrea Cardy Bailey <andrea.c.bailey@gmail.com>
2019-09-30 11:24:24 -0400

    Updating typo

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[yourname]';
 
-const getItems() {
+const getItems = function () {
   return Promise.resolve('A successful response!');
-}
+};
 
 export default {
   getItems
